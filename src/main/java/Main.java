d6225b93bd263645d421d3437d16879b31787b79
megoRU <savin.yura2014@gmail.com>
2020-08-28 18:42:41 +0300

    add folder local save image

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,31 +22,39 @@ public class Main {
   private static final String remoteHost = "95.181.157.159";
   private static final String username = "";
   private static final String password = "";
+  private static final String localFolderName = "C:\\MayningImagesFolder";
   private static final String folderSFTP = "/var/www/vhosts/megolox.ru/httpdocs/mayningImages/";
 
   public static void main(String[] args) {
     for (; ; ) {
       try {
-        Document doc = Jsoup.connect("https://megolox.ru/mayning/").data("query", "Java").userAgent(
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
-            .cookie("auth", "token").get();
-        Elements h1Elements = doc.select("h2");
-        String h2 = h1Elements.text();
+        File directory = new File(localFolderName);
+        if(!directory.exists()){
+          directory.mkdir();
+        }
+        if (directory.exists()) {
+          Document doc = Jsoup.connect("https://megolox.ru/mayning/").data("query", "Java")
+              .userAgent(
+                  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
+              .cookie("auth", "token").get();
+          Elements h1Elements = doc.select("h2");
+          String h2 = h1Elements.text();
 
-        Robot robot = new Robot();
-        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(
-            Toolkit.getDefaultToolkit().getScreenSize()));
-        Calendar now = Calendar.getInstance();
-        ImageIO.write(screenShot, "JPG",
-            new File("src/main/resources/" + formatter.format(now.getTime()) + ".jpg"));
-        System.out.println(formatter.format(now.getTime()));
-        ChannelSftp channelSftp = setupJsch();
-        channelSftp.connect();
-        channelSftp.put("src/main/resources/" + formatter.format(now.getTime()) + ".jpg",
-            folderSFTP + formatter.format(now.getTime()) + ".jpg");
-        channelSftp.exit();
-        Files.delete(Path.of("src/main/resources/" + formatter.format(now.getTime()) + ".jpg"));
-        Thread.sleep(Long.parseLong(h2));
+          Robot robot = new Robot();
+          BufferedImage screenShot = robot.createScreenCapture(new Rectangle(
+              Toolkit.getDefaultToolkit().getScreenSize()));
+          Calendar now = Calendar.getInstance();
+          ImageIO.write(screenShot, "JPG",
+              new File(localFolderName + "\\" + formatter.format(now.getTime()) + ".jpg"));
+          System.out.println(formatter.format(now.getTime()));
+          ChannelSftp channelSftp = setupJsch();
+          channelSftp.connect();
+          channelSftp.put(localFolderName + "\\" + formatter.format(now.getTime()) + ".jpg",
+              folderSFTP + formatter.format(now.getTime()) + ".jpg");
+          channelSftp.exit();
+          Files.delete(Path.of(localFolderName + "\\" + formatter.format(now.getTime()) + ".jpg"));
+          Thread.sleep(Long.parseLong(h2));
+        }
       } catch (Exception ex) {
         ex.printStackTrace();
       }
